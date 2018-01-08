b3f398f1d972c8bcbd25debc0d19d6eeed2299a0
Ariel Fonticiella <ariel@fonticiella.io>
2018-01-08 12:43:39 -0500

    Completed story maker

---
+++ Treehouse/StoryMaker/story.js
@@ -0,0 +1,13 @@
+var character = prompt("Please provide a 'Character' for your story:");
+
+var setting = prompt("Please provide a 'Setting' where your story takes place:");
+
+var placeToTravel = prompt("Please provide a 'Place to Travel' for your story:");
+
+var problem = prompt("Please provide a 'Problem' for your story:");
+
+var adjective = prompt("Please provide a 'Adjective' for your story:");
+
+var verb = prompt("Please provide a 'Verb' for your story:");
+
+document.write("Once upon a time, " + character + " lived in an old and smelly " + setting +". He/she wanted to go somewhere exciting, so he/she planned a trip to " + placeToTravel +". But then, " + problem + ". " + character + " was very " + adjective + " and " + adjective + ". He/she had to do something! So he/she " + verb + ". After this, " + character + " lived happily ever after.");
