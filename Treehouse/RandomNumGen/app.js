1317d9fbb7a9f9b69000d016e345aa59936d06d2
Ariel Fonticiella <ariel@fonticiella.io>
2018-01-09 13:01:00 -0500

    finished random number gen b/w two numbers.

--- Treehouse/RandomNumGen/app.js
+++ Treehouse/RandomNumGen/app.js
@@ -1,5 +1,13 @@
-var userNum = parseInt( prompt("Please provide a number:") );
+var userNum1 = prompt("Please provide a starting number:");
 
-var randomNum = Math.floor( Math.random() * userNum );
+var bottomNum = parseInt(userNum1);
 
-document.write("Your Random Number is " + randomNum);
+var userNum = prompt("Please provide a number:");
+
+var topNum = parseInt(userNum);
+
+var randomNum = Math.floor( Math.random() * (topNum - bottomNum + 1)) + bottomNum;
+
+var message = "<p>" + randomNum + " is a number between " + bottomNum + " and " + topNum + ".</p>";
+
+document.write(message);
