f360b3c8e53566e03d4bc53eea7fb9170c95d812
Ariel Fonticiella <ariel@fonticiella.io>
2018-01-10 09:46:15 -0500

    created a simple quiz

---
+++ Treehouse/quiz/quiz.js
@@ -0,0 +1,7 @@
+var answer = prompt("Where am I from?");
+
+if ( answer.toUpperCase() === "MIAMI" ) {
+  document.write("<p> That's right!</p>");
+} else {
+  document.write("<p> Nope! Try again!</p>");
+}
