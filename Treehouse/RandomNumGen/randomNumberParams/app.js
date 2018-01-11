dd8e5c50e8b3e2904360b6b8e331b5cd07e9ff4e
Ariel Fonticiella <ariel@fonticiella.io>
2018-01-11 12:38:11 -0500

    added error msg

--- Treehouse/RandomNumGen/randomNumberParams/app.js
+++ Treehouse/RandomNumGen/randomNumberParams/app.js
@@ -1,5 +1,8 @@
 function randomNumber( lower , upper ) {
+  if ( isNaN(lower) || isNaN(upper) ) {
+    throw new Error ("Please provide a valid number between 0 - 9");
+  }
   return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
 
-alert ( randomNumber( 40, 90 ) );
+console.log ( randomNumber (20 , 50) );
