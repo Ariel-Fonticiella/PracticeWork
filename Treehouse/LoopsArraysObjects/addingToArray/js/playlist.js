edb3ca8f4209fa88b5308939c1ccac723431e6f0
Ariel Fonticiella <ariel@fonticiella.io>
2018-01-19 21:14:01 -0500

    Added more items

--- Treehouse/LoopsArraysObjects/addingToArray/js/playlist.js
+++ Treehouse/LoopsArraysObjects/addingToArray/js/playlist.js
@@ -6,6 +6,10 @@ var playlist = [ ];
 // Here, we add an item to the end of the array with the PUSH() METHOD
 playlist.push('I Did It My Way');
 
+// The PUSH() METHOD can accept multiple arguments, each argument is one item
+// that is added to the array.
+playlist.push('Gimmie The Loot', 'We Got The Jazz', 'The Story Of O.J.');
+
 // Here, we use a PRINTLIST() FUNCTION. This function is in js/helper.js. It
 // takes an array as an argument and prints it out as an ordered list in HTML
 printList( playlist );
