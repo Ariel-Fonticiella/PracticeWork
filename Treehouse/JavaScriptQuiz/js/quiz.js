a563d633e46be1429fc42fe8d65c4d5a11ae55a3
Ariel Fonticiella <ariel@fonticiella.io>
2018-01-25 16:16:36 -0500

    added function that builds html ordered list for me with loop.

--- Treehouse/JavaScriptQuiz/js/quiz.js
+++ Treehouse/JavaScriptQuiz/js/quiz.js
@@ -8,28 +8,56 @@ var theQuiz = [
     ["What avenue did Ariel grow up on?", "132"]
 ];
 
-var html;
-
-// Created a variable to keep count of the answers the user answers correctly
+// Created a variable to keep count of the answers the user answers correctly.
 var correctAnswerCount = 0;
 
-// Created a variable to keep count of the answers the user answers incorrectly
+// Created a variable to store the question the user got correct.
+var correct = [];
+
+// Created a variable to keep count of the answers the user answers incorrectly.
 var wrongAnswerCount = 0;
+
+// Created a variable to store the question the user got incorrect.
+var wrong = [];
+
+// Created a variable to store the questions that were correct/incorrect to display
+var question = [];
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  // .innerHTML is a PROPERTY you can change just like you can change the value in any variable by simply setting the property.
+  outputDiv.innerHTML = message;
+}
+
+// To conver these arrays to an HTML list, I create a function that accepts an array as a parameter, then loops through that array constructing the HTML for an ordered list.
+  function buildList(arr) {
+    // Created a variable that adds to the HTML we want to make. It uses a loop to create an <li></li> tag with w/e is in the array at the specified index position.
+  var listHTML = '<ol>';
+    for(var i = 0; i < arr.length; i +=1) {
+      listHTML += '<li>' + arr[i] + '</li>';
+    }
+    listHTML += '</ol>';
+    return listHTML;
+}
 
 // Created a loop to loop through the length of theQuiz array.
 for (var i = 0; i < theQuiz.length; i += 1 ) {
 
+      question = theQuiz[i][0];
+      answer = theQuiz[i][1];
+
       // When the loop is run, create a variable that stores the user's response
-      var userAnswer = prompt(theQuiz[i][0]).toLowerCase();
+      var userAnswer = prompt(question).toLowerCase();
 
       // Then create a variable that stores the correct answer from the array and sets all responses to be in lowercase. I did this becuase we do not know how the user will type the answer into the prompt, this ensures that if the answer is correct, it will be marked as correct regardless of what case the answer was written in.
-      var correctAnswer = theQuiz[i][1].toLowerCase();
+      var correctAnswer = answer.toLowerCase();
 
       // Create a conditional statement to test the user's answer with the correct answer.
       if (userAnswer === correctAnswer) {
 
           // If the condition is true, the user is correct and we add 1 to our correctAnswerCount.
           correctAnswerCount += 1;
+          correct.push(question);
 
           // Alert the user that they are correct and tell them how many quesitons they have gotten correct so far.
           alert("You are correct! The answer was '" + correctAnswer + "!' So far you have gotten " + correctAnswerCount + " question(s) correct out of " + theQuiz.length + "!");
@@ -38,6 +66,7 @@ for (var i = 0; i < theQuiz.length; i += 1 ) {
 
           // If the condition is false, the user is incorrect and we add 1 to our incorrectAnswerCount.
           wrongAnswerCount += 1;
+          wrong.push(question);
 
           // Alert the user that they are incorrect, give them the correctAnswer, and tell them how many quesitons they have gotten incorrect so far.
           alert("Sorry! The correct answer was actually '" + correctAnswer + "'. So far you have got " + wrongAnswerCount + " question(s) incorrect.");
@@ -46,14 +75,11 @@ for (var i = 0; i < theQuiz.length; i += 1 ) {
 
 } // Loop-End
 
-function print(message) {
-  var outputDiv = document.getElementById('output');
-  // .innerHTML is a PROPERTY you can change just like you can change the value in any variable by simply setting the property.
-  outputDiv.innerHTML = message;
-}
+  html = "<h1> There was a total of <strong>" + theQuiz.length + " question(s)</strong> on this test.</h1>";
+  html += "<h1> You answered a total of <strong>" + correctAnswerCount + " question(s) correctly:</strong> These are the questions: </h1>";
+  html += buildList(correct);
 
-html = [
-    "<h1> There was a total of <strong>" + theQuiz.length + " question(s)</strong> on this test. </h1> <br /> <h1> Of those "+ theQuiz.length +" quesitons, you answered a total of <strong>" + correctAnswerCount + " question(s) correctly</strong>. </h1> <br /> <h2> And you answered <strong>" + wrongAnswerCount + " question(s) incorrectly</strong>. </h2>"
-  ];
+  html += "<h1> And you answered <strong>" + wrongAnswerCount + " question(s) incorrectly</strong>. These are the questions: </h1>";
+  html += buildList(wrong);
 
   print(html);
