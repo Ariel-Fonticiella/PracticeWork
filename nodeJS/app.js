00f89a81883e52a56ce96ba2fee8017ba2579e90
Ariel Fonticiella <ariel@fonticiella.io>
2018-02-22 21:08:15 -0500

    practice with node

--- nodeJS/app.js
+++ nodeJS/app.js
@@ -1,5 +1,8 @@
 var greet = require('./greet');
 // the require function will return modules.exports on greet.js.
+// if it can't find a greet.js file, it will look for a folder called greet and
+// look for an index.js file to return.
 
-greet();
+greet.english();
+greet.spansih();
 // Now you can call 'greet()' because you have your own variable for greet that is set equal to the results of 'require'.
