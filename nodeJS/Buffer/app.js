1a8697d0ce31ac051bba9ae039b532403bc3f2c7
Ariel Fonticiella <ariel@fonticiella.io>
2018-02-26 16:59:33 -0500

    understanding buffer

---
+++ nodeJS/Buffer/app.js
@@ -0,0 +1,5 @@
+var buf = new Buffer('Hello', 'utf8');
+// take this string 'Hello' and convert it to binary data. To convert it, use the 'utf8' encoding.
+console.log(buf);
+console.log(buf.toString());
+console.log(buf.toJSON());
